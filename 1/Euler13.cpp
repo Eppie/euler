c7bf07bae95cd55b45929a07d30fa210fe8c7a32
Andrew Epstein <aepstein@sevone.com>
2015-07-31 16:52:32 -0400

    Added problem 13 template, testing ssh keys

---
+++ 1/Euler13.cpp
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Euler13.cpp
+ *
+ *    Description:  Solution to Project Euler, Problem 13
+ *
+ *        Version:  1.0
+ *        Created:  07/31/2015 4:51:15 PM
+ *       Revision:  none
+ *       Compiler:  g++
+ *
+ *         Author:  Andrew Epstein
+ *   Organization:  SevOne
+ *        Problem:  Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
+ *         Answer:  5537376230
+ * =====================================================================================
+ */
+
+#include "../helper.hpp"
+
+int main() {
+	return 0;
+}
