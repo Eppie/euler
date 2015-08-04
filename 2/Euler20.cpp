920eefab0f3881c6f5a1f436997a1b9b548d1bf6
Andrew Epstein <aepstein@udel.edu>
2015-08-03 23:39:51 -0400

    Finished Problem 20.

--- 2/Euler20.cpp
+++ 2/Euler20.cpp
@@ -17,8 +17,35 @@
  * =====================================================================================
  */
 
+#define NUMDIGITS 160
+
 #include "../helper.hpp"
 
+vector<int> factorial( int n ) {
+	vector<int> digits(NUMDIGITS, 0);
+	int size = 1;
+	digits[0] = 1;
+
+	for( int i = 2; i <= n; i++ ) {
+		int carry = 0;
+
+		for( int j = 0; j < size; j++ ) {
+			int prod = digits[j] * i + carry;
+			digits[j] = prod % 10;
+			carry = prod / 10;
+		}
+
+		while( carry ) {
+			digits[size] = carry % 10;
+			carry /= 10;
+			size++;
+		}
+	}
+
+	return digits;
+}
+
 int main() {
+	cout << sum( factorial(100) ) << endl;
 	return 0;
 }
