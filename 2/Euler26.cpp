8fa88d3023119540a02cc1f2c55a5376cad1736e
Andrew Epstein <aepstein@sevone.com>
2015-12-18 22:13:19 -0500

    Added framework for Problem 26.

---
+++ 2/Euler26.cpp
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Euler26.cpp
+ *
+ *    Description:  Solution to Project Euler, Problem 26
+ *
+ *        Version:  1.0
+ *        Created:  10/25/2015 03:59:08 PM
+ *       Revision:  none
+ *       Compiler:  g++
+ *
+ *         Author:  Andrew Epstein
+ *   Organization:  SevOne
+ *        Problem:  Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
+ *         Answer:  983
+ * =====================================================================================
+ */
+
+#include "../helper.hpp"
+
+int main() {
+	return -1;
+}
