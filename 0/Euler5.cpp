9944326585a178a054b711606b40e47deb38e3e8
Andrew Epstein <aepstein@udel.edu>
2016-09-24 15:42:44 -0400

    Faster algorithm for #5

--- 0/Euler5.cpp
+++ 0/Euler5.cpp
@@ -5,13 +5,14 @@
  *
  *    Description:  Solution to Project Euler, Problem 5
  *
- *        Version:  1.0
+ *        Version:  1.1
  *        Created:  07/29/2015 11:11:11 AM
  *       Revision:  none
  *       Compiler:  g++
  *
  *         Author:  Andrew Epstein
- *        Problem:  2520 is the smallest number that can be divided by the numbers 1-10 without any remainder. What is the smallest positive number that is evenly divisible by all of the numbers 1-20?
+ *        Problem:  2520 is the smallest number that can be divided by the numbers 1-10 without any remainder.
+ *                  What is the smallest positive number that is evenly divisible by all of the numbers 1-20?
  *         Answer:  232792560
  *
  * =====================================================================================
@@ -20,20 +21,26 @@
 #include "../helper.hpp"
 
 int solve5() {
-	int test = 2520;
-	int sum;
+	int increment = 1;
+	int furthestDivisor = 1;
+	int result = 1;
+	int limit = 20;
 
 	while( true ) {
-		sum = 0;
+		int counter = 1;
 
-		for( int i = 11; i <= 20; i++ ) {
-			sum += test % i;
+		while( counter <= limit + 1 && result % counter == 0 ) {
+			if ( counter > furthestDivisor ) {
+				furthestDivisor = counter;
+				increment = result;
+			}
+			++counter;
 		}
 
-		if( sum == 0 ) {
-			return test;
+		if( counter == limit + 2 ) {
+			return result;
+		} else {
+			result += increment;
 		}
-
-		test += 2520;
 	}
 }
