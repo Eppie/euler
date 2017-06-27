d474cd896217a0a3b02f5f137ab4214db47f2ba5
Andrew Epstein <aepstein@udel.edu>
2017-06-26 23:03:37 -0400

    Added the commas back in

--- main.cpp
+++ main.cpp
@@ -412,7 +412,7 @@ int main( int argc, char* argv[] ) {
 
 	auto end = chrono::high_resolution_clock::now();
 	stringstream ss;
-	//ss.imbue( locale( "" ) );
+	ss.imbue( locale( "" ) );
 	ss << fixed << chrono::duration_cast<chrono::microseconds>( end - begin ).count() << "μs";
 	cout << ss.str() << endl;
 
